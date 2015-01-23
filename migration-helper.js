d1202cd3a426e321dd755ab5a09518411c316385
Chris Joel <cdata@google.com>
2015-01-22 17:25:07 -0800

    WIP

--- migration-helper.js
+++ migration-helper.js
@@ -1,19 +1,1 @@
-var Polymerfill = {
-  configure: function (target) {
-    var defaults = target.defaults || {};
 
-    Object.keys(defaults).forEach(function (key) {
-      var value = defaults[key];
-
-      if (Array.isArray(value)) {
-        value = value.slice();
-      } else if (value instanceof Object) {
-        value = Object.create(value);
-      }
-
-      if (target[key] === undefined) {
-        target[key] = value;
-      }
-    });
-  }
-};
